cc71d76cc49932f9bc7b4811337cbedf4b670a27
Ciolv <73327363+Ciolv@users.noreply.github.com>
2022-06-06 18:25:16 +0200

    Amendment

--- src/main/java/Plugin/Civilian/Person/Characteristic.java
+++ src/main/java/Plugin/Civilian/Person/Characteristic.java
@@ -7,11 +7,12 @@ public enum Characteristic {
     CONFUSED(0.8),
     HECTIC(0.6),
     SLOW(0.7),
+    DEFAULT(1),
     FAST(2),
     CALM(3.3),
     TARGET_ORIENTED(5),
     BUSSY(2.5);
-    
+
 
     public double value;
     private Characteristic(double value){
@@ -26,6 +27,16 @@ public enum Characteristic {
         for (Characteristic type :
                 Characteristic.values()) {
             if (value == type.value) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+    public static Characteristic fromName(String name) {
+        for (Characteristic type :
+                Characteristic.values()) {
+            if (name.equals(type.name())) {
                 return type;
             }
         }
